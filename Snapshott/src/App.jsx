83f02b79465062a7fd615d883052e8aed653f07b
Sawda-Hassan <Milgomaariin@gmail.com>
2024-07-31 15:42:09 +0300

    Initial commit

--- Snapshott/src/App.jsx
+++ Snapshott/src/App.jsx
@@ -1,44 +1,105 @@
-import React,{ useState } from 'react'
 import axios from 'axios';
-import Gallery from './component/Gallery';
-const apiKey = "636e1481b4f3c446d26b8eb6ebfe7127";
-  const App =() =>{
-    const [search,setSearch] = useState("");
-    const [data,setData] = useState([]);
-    const changeHandler = e =>{
-      setSearch(e.target.value);
-    }
-    const submitHandler = e =>{
-      e.preventDefalt();
-      axios.get(
-      `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${search}&per_page=24&format=json&nojsoncallback=1`
-    )
-    .then(response =>{
-   setData(response.data.photes.photo)
-    })
-    .catch(error => {
-      console.log(
-        "Encountered an error with fetching and parsing data",
-        error
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
-      );
-    })
-    
+const API_URL = 'https://api.unsplash.com/search/photos';
+const IMAGES_PER_PAGE = 20;
+function App() {
+  const searchInput = useRef(null);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const fetchImages = useCallback(async () => {
+    try {
+      if (searchInput.current.value) {
+        setErrorMsg('');
+        setLoading(true);
+        const { data } = await axios.get(
+          `${API_URL}?query=${
+            searchInput.current.value
+          }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
+            import.meta.env.VITE_API_KEY
+          }`
+        );
+        setImages(data.results);
+        setTotalPages(data.total_pages);
+        setLoading(false);
+      }
+    } catch (error) {
+      setErrorMsg('Error fetching images. Try again later.');
+      console.log(error);
+      setLoading(false);
     }
+  }, [page]);
+
+  useEffect(() => {
+    fetchImages();
+  }, [fetchImages]);
+
+  const resetSearch = () => {
+    setPage(1);
+    fetchImages();
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    resetSearch();
+  };
+
+  const handleSelection = (selection) => {
+    searchInput.current.value = selection;
+    resetSearch();
+  };
+
   return (
-    <div>
-      <center>
-        <h2>Gallery Snapshoots</h2><br></br>
-        <form onSubmit={submitHandler}>
-        <input size="30" type="text" value={search} onChange={changeHandler} /> <br/><br/>
-        <input type="submit" name="Search" />
-        </form>
-        <br/>
-        {data.length>=1?<Gallery data={data}/>:<h4>No Data loaded</h4>}
-      </center>
+    <div className='container'>
+      <h1 className='title'>Image Search</h1>
+      {errorMsg && <p className='error-msg'>{errorMsg}</p>}
+      <div className='search-section'>
+        <Form onSubmit={handleSearch}>
+          <Form.Control
+            type='search'
+            placeholder='Type something to search...'
+            className='search-input'
+            ref={searchInput}
+          />
+        </Form>
+      </div>
+      <div className='filters'>
+        <div onClick={() => handleSelection('nature')}>Nature</div>
+        <div onClick={() => handleSelection('birds')}>Birds</div>
+        <div onClick={() => handleSelection('cats')}>Cats</div>
+        <div onClick={() => handleSelection('shoes')}>Shoes</div>
+      </div>
+      {loading ? (
+        <p className='loading'>Loading...</p>
+      ) : (
+        <>
+          <div className='images'>
+            {images.map((image) => (
+              <img
+                key={image.id}
+                src={image.urls.small}
+                alt={image.alt_description}
+                className='image'
+              />
+            ))}
+          </div>
+          <div className='buttons'>
+            {page > 1 && (
+              <Button onClick={() => setPage(page - 1)}>Previous</Button>
+            )}
+            {page < totalPages && (
+              <Button onClick={() => setPage(page + 1)}>Next</Button>
+            )}
+          </div>
+        </>
+      )}
     </div>
-    
-  )
+  );
 }
 
 export default App;
